8161fccc7d2f59e2f60f05ca8ade7214dd2abd5d
Vianney Ruhlmann <ruhlmannvianney@gmail.com>
2022-04-18 13:52:57 +0200

    deprecated option for treesitter

--- init.lua
+++ init.lua
@@ -90,8 +90,8 @@ require('neoscroll').setup()
 --- Treesitter
 
 require('nvim-treesitter.configs').setup {
-    -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = "maintained",
+    -- One of "all" or a list of languages
+    ensure_installed = "all",
 
     -- Install languages synchronously (only applied to `ensure_installed`)
     sync_install = false,
